e1c98a735ea8b0ee1bf96d29481269bf5376cc77
sujal-kalmegh <sujalkalmegh23@gmail.com>
2026-06-05 05:51:17 +0530

    Main

--- SetMatrixZero.java
+++ SetMatrixZero.java
@@ -50,6 +50,13 @@ class SetMatrixZero{
     }
 
     public static void main(String[] args) {
-        
+        int[][] arr = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        setMatrix(arr);
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
